98d2092d75c85a11a09218968abefa91149c4872
Taras-Lisnyak <taras4lisnyak@gmail.com>
2026-02-02 20:38:57 +0200

    fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,10 +4,7 @@ import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
-import { Votes, VoteType } from "../../types/votes"
-
-
-
+import type { Votes, VoteType } from "../../types/votes";
 
 
 export default function App() {
